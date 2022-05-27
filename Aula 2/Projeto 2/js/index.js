2cadd64c849a154dc3d60db838b0b6fb923d5308
Priscilla Prado <88402963+PriscillaPrado@users.noreply.github.com>
2022-05-27 12:33:54 -0300

    Mudança no nome das variaveis

---
+++ Aula 2/Projeto 2/js/index.js
@@ -0,0 +1,11 @@
+//Calcular o gasto de combustível em 1 mês. Sabendo que gasto 50 por dia, quanto será gasto em 5, 15 e 30 dias?
+
+let valorDia = 50;
+let gastoCincoDias = valorDia * 5;
+let gastoQuinzeDias = valorDia * 15;
+let gastoTrintaDias = valorDia * 30;
+
+let mensagem = "Eu gasto R$" + gastoCincoDias + "em cinco dias, R$" + Quinze + "em 15 dias e R$" + gastoTrintaDias + "em 30 dias."
+
+alert (mensagem);
+
