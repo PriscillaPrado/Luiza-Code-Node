c01c60cb79e9496863a97bc30d8c6a9650aadda3
Priscilla Prado <88402963+PriscillaPrado@users.noreply.github.com>
2022-05-28 13:57:51 -0300

    abstracao, comparaçao

---
+++ Aula 4/Comparação/js/index.js
@@ -0,0 +1,24 @@
+/*Resultado sempre será boolean
+
+Igualdade ===
+Não igualdade !==
+Maior que >
+Maior ou igual a >=
+Menor que <
+Menor ou igual a <= 
+
+Igualdade 1===1 - resultado true
+Não igualdade 1 !== 1 - false
+Maior que 1 > 1 - false 
+Maior ou igual a 1 >= 1 - true
+Menor que 1 < 1 - false
+Menor ou igual a 1 <= 1 - true
+
+*/
+
+console.log (1===1);
+console.log (1!==1);
+console.log (1 > 1);
+console.log (1 >= 1);
+console.log (1 < 1);
+console.log (1 <= 1);
